b2e25b74008a06a4decebbc90a73a2b1cff80661
ArturMak <34660203+ArturMak@users.noreply.github.com>
2017-12-21 20:54:29 +0200

    Add files via upload

---
+++ LemanRuss.java
@@ -0,0 +1,30 @@
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+
+public class LemanRuss extends Robot {
+
+    public LemanRuss(){
+
+    }
+    @Override
+    public void run () {
+        while (true) {
+            turnGunLeft(360);
+            ahead(400);
+            turnLeft(90);
+            }
+        }
+
+    @Override
+    public void onScannedRobot (ScannedRobotEvent event) {
+        fire(2);
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent event){
+        fire(3);
+        back(60);
+    }
+}
